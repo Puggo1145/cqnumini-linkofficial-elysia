6e0ac5ca4c4def2e25c4b309548a41510e25b861
Andrew Zhao <puggoo1145@gmail.com>
2024-09-20 17:54:36 +0800

    完成教务系统 cookie 和水电费查询 api 的单元测试

--- test/jwxt.test.ts
+++ test/jwxt.test.ts
@@ -2,11 +2,7 @@ import { describe, expect, it } from "bun:test";
 import { app } from "../src";
 
 const cookieOnTest = [
-    "CASTGC=TGT-130466-eyPDpcMSz9lZYc69BubSdAX0cbgM1CskmGwvHhKfcxLkgDH4ea-https://csxrz.cqnu.edu.cn/cas; Path=/cas/",
-    "CASTGC=TGT-130870-Df2syGc2hk4gFzHpfM7xwatYbA7qvBXxoqZCebetlqdyxnHWyn-https://csxrz.cqnu.edu.cn/cas; Path=/cas/",
-    "CASTGC=TGT-130696-ENLXwdndf0QIxkZO22aQnbDOkumz6t23cJUaEBi9RIWqb5PcrA-https://csxrz.cqnu.edu.cn/cas; Path=/cas/",
-    "CASTGC=TGT-130525-wERPtKuzrBXnGbliDoxZaWTNycqQ1aLFGvquz3HgY6P66Ek6iV-https://csxrz.cqnu.edu.cn/cas; Path=/cas/",
-    "CASTGC=TGT-130786-dWVoToNHwYRM4dw66wofCC5QjqeGgNWPuhEpcHqH4C41NceuH1-https://csxrz.cqnu.edu.cn/cas; Path=/cas/",
+    ""
 ]; 
 
 
